7b09cd60a4d6db919a2e8e52d467df65898818eb
riefive <rie_five@yahoo.com>
2018-08-14 15:19:15 +0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: './src/app.js',
@@ -11,8 +13,10 @@ module.exports = {
         publicPath: 'dist/'
     },
     node: {
-        __dirname: false
+        __dirname: false,
+        __filename: false,
     },
+    externals: [ nodeExternals() ],
     module: {
         rules: [
             {
@@ -20,6 +24,28 @@ module.exports = {
                 exclude: /(node_modules)/,
                 test: /\.js$/
             }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    ecma: 6,
+                    beautify: false,
+                    compress: true,
+                    comments: false,
+                    minify: true,
+                    mangle: true,
+                    toplevel: false,
+                    keep_classnames: false,
+                    keep_fnames: false,
+                    output: {
+                        comments: false
+                    }
+                }
+            }),
         ]
     }
 }
